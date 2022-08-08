bd7ac653f69358a5a349959bbfe7c228fbaf66f6
rajiya11 <rajiyabshaik4@gmail.com>
2022-08-08 13:47:05 +0530

    Initial commit

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,7 +1,7 @@
 
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("helloooo");
+		System.out.println("ffffhelloooo");
 		System.out.println("!1helloooo");
 	}
 
