059bdbfef9ba64187576af49d43f5271a8ab7112
rajiya11 <rajiyabshaik4@gmail.com>
2022-08-08 13:52:35 +0530

    bug changed

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,6 +3,8 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("ffffhelloooo");
 		System.out.println("!1helloooo");
+		//gug 1222
+		//logic pending
 	}
 
 }
